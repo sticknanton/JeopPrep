1c245a47c8d252f8dd0f17b795b97a597a5284e5
nick stanton <nick.a.stanton@gmail.com>
2016-01-18 14:04:46 -0500

    rough answer form modal

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 console.log('hello is it me you\'r looking for');
 function renderDoubleJeopardy(data) {
-// to be done later...
+
 }
 function renderJeopardy(data) {
   var source = $('#game-template').html();
@@ -32,13 +32,92 @@ function getGame(){
   });
 }
 function renderTvListener() {
-  $('body').on('click', '.clue', function () {
+  $('body').on('click', '.clue', function (e) {
+    e.preventDefault();
+    clue = $(this);
+    modal.open({content: "<h3>"+clue.find(".question").text()+"</h3>"});
 
-    var clue = $(this);
-    console.log(clue);
+
   })
+
+
+
+  $('form#answer').on('submit',function(e){
+    e.preventDefault();
+    console.log(clue.find(".answer").text());
+    answer = $(this).find("input[name='answer']").val();
+    console.log(answer);
+    if (answer == clue.find(".answer").text()) {
+      console.log('correct!');
+    }
+    modal.close();
+
+
+  });
 }
-$(function(){
+
+
+
+	var modal = (function(){
+		var
+		method = {},
+		$overlay,
+		$modal,
+		$content,
+		$close;
+
+		method.center = function () {
+			var top, left;
+			top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+			left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+			$modal.css({
+				top:top + $(window).scrollTop(),
+				left:left + $(window).scrollLeft()
+			});
+		};
+
+		method.open = function (settings) {
+			$content.empty().append(settings.content);
+			$modal.css({
+				width: settings.width || 'auto',
+				height: settings.height || 'auto'
+			});
+
+			method.center();
+			$(window).bind('resize.modal', method.center);
+			$modal.show();
+			$overlay.show();
+		};
+
+		method.close = function () {
+			$modal.hide();
+			$overlay.hide();
+		};
+
+		$overlay = $('<div id="overlay"></div>');
+		$modal = $('<div id="modal"></div>');
+		$content = $('<div id="content"></div>');
+    $form = $('<form id="answer">')
+      $form.append($('<input type="text" name="answer" placeholder="Answer">'))
+      $form.append($('<input type="submit" value="Submit">'))
+
+		$modal.hide();
+		$overlay.hide();
+		$modal.append($content, $close, $form);
+
+    $(document).ready(function(){
+
+      $('body').append($overlay, $modal);
+
+    });
+
+
+
+		return method;
+	}());
+
+	// Wait until the DOM has loaded before querying the document
+  $(function(){
   getGame();
   renderTvListener();
 });
