2b8123e00df03a7aadd525441447f08b17ac2db7
Graig Janssen <graig.janssen@gmail.com>
2016-01-19 10:22:13 -0500

    Adds custom middleware and updates app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// MODULES //
+
 var express = require('express');
 var app = express();
 
@@ -9,11 +11,21 @@ app.use(express.static('./public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded());
 
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.set('view engine', 'ejs');
 
 var mongoPath = 'mongodb://localhost/jepquestions';
 var mongoose = require('mongoose');
 mongoose.connect(mongoPath);
+
+// CUSTOM MIDDLEWARE //
+
+var loadUser = require('./middleware/loadUser');
+app.use(loadUser);
+
+// ROUTES //
 
 var index = require('./routes/index');
 app.use('/', index);
@@ -24,7 +36,7 @@ app.use('/api/users', users);
 var clues = require('./routes/clues');
 app.use('/api/clues', clues);
 
-
+// LISTEN //
 
 var port = 8080;
 app.listen(port, function(){
---
+++ middleware/loadUser.js
@@ -0,0 +1,13 @@
+var User = require('../models/user');
+
+function loadUser(req, res, next){
+  if (req.cookies.token){
+    User.findOne({token: req.cookies.token}, function(err, dbUser){
+      if (err) { return err; }
+      req.user = dbUser;
+      next();
+    });
+  } else {
+    next();
+  }
+}
