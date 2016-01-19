1c5f1333bc1b34d67f54d79b22cb9528d0134c81
nick stanton <nick.a.stanton@gmail.com>
2016-01-19 10:27:26 -0500

    stage

--- routes/clues.js
+++ routes/clues.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Clue = require('../models/clue');
 router.get('/', function(req, res){
-  var random = (Math.floor((Math.random() * 3301) + 3000)).toString();
+  // var random = (Math.floor((Math.random() * 3301) + 3000)).toString();
   //good show numbers 3677
   Clue.find({ show_number: random }, function(err, dbClues){
     res.json(dbClues);
